6f54d3fc257986f40e7836cfd183b9248373f981
诗雅 周 <zhoushiya@126.com>
2021-02-07 17:19:06 +0800

    初始化提交，控制台示例完成

---
+++ Net5Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using WebApi.EF;
+
+namespace Net5Demo
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+
+            var options = new DbContextOptions<SalerDbContext>();
+
+            await using var context = new SalerDbContext(options);
+            await Write(context);
+            await Read(context);
+
+        }
+
+        static async Task Read(SalerDbContext context)
+        {
+            var saler_accounts = await context.SalerAccount.AsNoTracking().ToListAsync();
+            saler_accounts.ForEach(account => Console.WriteLine(account.Account));
+        }
+
+        static async Task Write(SalerDbContext context)
+        {
+            await context.SalerAccount.AddAsync(new Saler.Core.Entities.SalerAccount
+            {
+                Account = "老周",
+                Id = -1
+            });
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ WebApi.EF/SalerDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saler.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools;
+
+namespace WebApi.EF
+{
+    public class SalerDbContext : DbContext
+    {
+        public SalerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SalerAccount> SalerAccount { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //判断当前数据库是Oracle 需要手动添加Schema(DBA提供的数据库账号名称)
+            if (this.Database.IsOracle())
+            {
+                // 设定当前默认table space
+                modelBuilder.HasDefaultSchema("SALER");
+            }
+
+            modelBuilder.Entity<SalerAccount>(entity =>
+            {
+                entity.Property(e => e.Id).UseHiLo(Consts.SEQ_SALER_ID);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle("User Id=saler;password=welcome;Data Source=localhost:1521/salepdb;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
